7d2cbf6116414a2ba83ea6f80682aec90d32f1d9
Tony <hzyhzyq@hotmail.com>
2022-03-19 22:20:41 +0800

    Mar. 19th

--- video-website-dao/src/main/java/com/yunqi/videowebsite/dao/DemoDao.java
+++
@@ -1,9 +0,0 @@
-package com.yunqi.videowebsite.dao;
-
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-
-@Mapper
-public interface DemoDao {
-    Long query(@Param("id") Long id);
-}
